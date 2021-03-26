2a248a22825d6e37f04d83d9392fea55457bda41
Mary Perry <mlab@Marys-MacBook-Pro.local>
2021-03-26 18:38:35 -0400

    added missing fields to Artist

--- starter_code/app.py
+++ starter_code/app.py
@@ -61,6 +61,11 @@ class Artist(db.Model):
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    website = db.Column(db.String(120))
+    # area_id = db.Column(db.Integer, db.ForeignKey('area.id'))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String)
+    # shows = db.relationship('Show', backref="gigs", lazy=True)
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
